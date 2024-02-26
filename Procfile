73072cd64a19e08f6756f5b3e630207341b1a958
mkamsani <mkamsani@posteo.org>
2024-02-26 23:00:25 +0800

    chore: final cleanup for project submission

--- Procfile
+++
@@ -1,2 +0,0 @@
-frontend: cd frontend && npm run dev
-backend: cd backend && go run . --schema $SCHEMA
--- deliverables/tmp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-drawio -x PrototypeSlides.drawio -o PrototypeSlides.png
-git add ./*
-git commit -m 'drawio: update deliverable diagrams'
-git commit
